9a66f41d7b90dca10c04e3780459938407cbcfcb
sacha-bouchez <sacha.bouchez@yahoo.com>
2026-01-17 08:23:45 +0000

    [metrics.py] add masking in PSNR and SSIM

--- tools/image/metrics.py
+++ tools/image/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tools.image.processing import normalize
 
-def PSNR(I, K):
+def PSNR(I, K, mask=None):
     """
     Compute Peak Signal-to-Noise Ratio (PSNR) between two images.
     I, K: np.ndarray
@@ -10,19 +10,18 @@ def PSNR(I, K):
         psnr: float
             The PSNR value in decibels (dB).
     """
-    if not 0 <= np.min(I) <= np.max(I) <= 1:
-        I = normalize(I)
-    if not 0 <= np.min(K) <= np.max(K) <= 1:
-        K = normalize(K)
+    if mask is not None:
+        I =np.where(mask, I, np.nan)
+        K =np.where(mask, K, np.nan)
     #
-    mse = np.mean((I - K) ** 2)
+    mse = np.nanmean((I - K) ** 2)
     if mse == 0:
         return float('inf')
-    max_pixel = np.max(I)
+    max_pixel = np.nanmax(I)
     psnr = 20 * np.log10(max_pixel / np.sqrt(mse))
     return psnr
 
-def SSIM(img1, img2, K1=0.01, K2=0.03, window_size=11, L=None):
+def SSIM(img1, img2, K1=0.01, K2=0.03, window_size=11, L=None, mask=None):
     """
     Compute Structural Similarity Index (SSIM) between two images.
     img1, img2: np.ndarray
@@ -39,22 +38,24 @@ def SSIM(img1, img2, K1=0.01, K2=0.03, window_size=11, L=None):
         ssim: float
             The SSIM value.
     """
-    if not 0 <= np.min(img1) <= np.max(img1) <= 1:
-        img1 = normalize(img1)
-    if not 0 <= np.min(img2) <= np.max(img2) <= 1:
-        img2 = normalize(img2)
+    if mask is None:
+        mask = np.ones_like(img1, dtype=bool)
+    #
+    if mask is not None:
+        img1 =np.where(mask, img1, np.nan)
+        img2 =np.where(mask, img2, np.nan)
     #
     if L is None:
-        L = np.max([img1.max(), img2.max()]) - np.min([img1.min(), img2.min()])
+        L = np.nanmax([np.nanmax(img1), np.nanmax(img2)]) - np.nanmin([np.nanmin(img1), np.nanmin(img2)])
 
     C1 = (K1 * L) ** 2
     C2 = (K2 * L) ** 2
 
-    mu1 = np.mean(img1)
-    mu2 = np.mean(img2)
-    sigma1_sq = np.var(img1)
-    sigma2_sq = np.var(img2)
-    sigma12 = np.cov(img1.flatten(), img2.flatten())[0, 1]
+    mu1 = np.nanmean(img1)
+    mu2 = np.nanmean(img2)
+    sigma1_sq = np.nanvar(img1)
+    sigma2_sq = np.nanvar(img2)
+    sigma12 = np.cov(img1[mask].flatten(), img2[mask].flatten())[0, 1]
 
     ssim = ((2 * mu1 * mu2 + C1) * (2 * sigma12 + C2)) / ((mu1 ** 2 + mu2 ** 2 + C1) * (sigma1_sq + sigma2_sq + C2))
     return ssim
