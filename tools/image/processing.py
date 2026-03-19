bd106a70c5a9245248979f50c0e5dc10ccfbcaa1
sacha-bouchez <sacha.bouchez@yahoo.com>
2026-03-19 17:28:24 +0000

    [processing.py] add reverse_graysale

--- tools/image/processing.py
+++ tools/image/processing.py
@@ -18,3 +18,22 @@ def normalize(im: np.ndarray) -> np.ndarray:
     max_val = np.max(im)
     im_norm = (im - min_val) / (max_val - min_val)
     return im_norm
+
+def reverse_grayscale(im: np.ndarray) -> np.ndarray:
+    """
+    Reverse the grayscale of a numpy array.
+
+    Parameters
+    ----------
+    im : np.ndarray
+        Input image array.
+
+    Returns
+    -------
+    np.ndarray
+        Grayscale-reversed image array.
+    """
+    im_max = np.max(im)
+    im_min = np.min(im)
+    im_reversed = im_max + im_min - im
+    return im_reversed
